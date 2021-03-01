ef4363a09ef0442f84fe5cc820d0400344e71d3f
GJAA98 <79035604+GJAA98@users.noreply.github.com>
2021-03-01 16:42:23 -0400

    Add files via upload
    Un programa que pida al usuario 4 números, los memorice (utilizando un array), calcule su media aritmética y después muestre en pantalla la media y los datos tecleados.

---
+++ Practica_de_array_y_clases/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Practica_de_array_y_clases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] numeros = new float[4];
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Inserte un numero:");                
+                string valor = Console.ReadLine();                
+                numeros[i] = float.Parse(valor);
+            }
+
+            Console.WriteLine("Resultado:");
+            float sumatoria = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Numero " + i + ":" + numeros[i]);
+                sumatoria += numeros[i];
+            }
+            float media = sumatoria / 4;
+            Console.WriteLine("Media aritmentica:"+ media);            
+        }
+    }
+}
